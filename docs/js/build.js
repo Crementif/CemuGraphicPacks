c637077bc9aa03a674ca80d5b0b0fa4ddc36ae67
Crementif <milanhekje@gmail.com>
2018-02-13 22:58:45 +0100

    Split Javascript from HTML
    Due to Github CDN preventions I thought I had to include the script, but I got mixed up and recently found that I mixed that up.
    
    I've also commented/changed a small part of the code which is now explains stuff somewhat clearer.
    
    There's 1 major thing I've added, which is the compat fetch code which is now somewhat working, although it seems that the regex is causing issues.

---
+++ docs/js/build.js
@@ -0,0 +1,328 @@
+﻿var gameList = {};
+var selected = false;
+
+function textureRule(sectionName) {
+    this.sectionName = sectionName;
+    // MISC
+    this.vsyncFrequency = undefined;
+    // Filters
+    this.width = undefined;
+    this.height = undefined;
+    this.formats = undefined;
+    this.formatsExcluded = undefined;
+    this.tilemodes = undefined;
+    this.tilemodesExcluded = undefined;
+    // Overwrites
+    // An 'instruction' requires a instructionType, followed by the index and length of the characters from the file.
+    this.overwriteWidth = [undefined, 0, 0];
+    this.overwriteHeight = [undefined, 0, 0];
+    this.overwriteFormat = [undefined, 0, 0];
+}
+
+function generateReportIssueUrl(title, body) {
+    return reportUrl + "new?title=" + title + "&body=" + body;
+}
+
+function addGameList(folderName, gameTitle) { // Using templates to add new game entries.
+    document.getElementById("gameList").appendChild(document.importNode(document.querySelector('#gameEntryTemplate').content, true));
+    currentGameTemplate = document.getElementById("input_folderName").parentElement;
+    currentGameTemplate.getElementsByTagName("input")[0].id = "input_" + folderName;
+    currentGameTemplate.getElementsByTagName("input")[0].name = folderName;
+    currentGameTemplate.getElementsByTagName("label")[0].htmlFor = "input_" + folderName;
+    currentGameTemplate.getElementsByTagName("p")[0].id = "label_" + folderName;
+    gameList[folderName].displayName = gameTitle[0];
+    currentGameTemplate.getElementsByTagName("label")[0].innerText = gameTitle[0]; // The first name tag should always be the game name.
+    
+    // Try to avoid name overflows by approximately calculating the length and optionally change it's listed title.
+    if (document.getElementsByName("selectGames")[0].offsetWidth < currentGameTemplate.getElementsByTagName("label")[0].offsetWidth) {
+        if (gameTitle.length > 0) gameList[folderName].displayName = gameTitle[1]; // Use the second name if the first one is too long.
+    }
+    currentGameTemplate.getElementsByTagName("label")[0].innerText = gameList[folderName].displayName;
+    currentGameTemplate.getElementsByTagName("p")[0].innerText = gameList[folderName].displayName;
+}
+
+function filterGameList(searchString) {
+    searchResults = 0;
+    if (searchString.value.toLowerCase().indexOf("mario od") > -1) document.getElementById("logo").innerText = "SwEmu Graphic Packs"; // 'ha funny'
+    for (var game in gameList) {
+        for (i = 0; i < gameList[game].searchTags.length; i++) {
+            // Now compare it with the search string.
+            if (gameList[game].searchTags[i].toLowerCase().indexOf(searchString.value.toLowerCase()) > -1) {
+                document.getElementsByName(game)[0].parentElement.style = "";
+                searchResults += 1;
+                break; // Found 1 tag that matched. Quit looping this game entry.
+            }
+            else {
+                document.getElementsByName(game)[0].parentElement.style = "display: none;";
+            }
+        }
+    }
+    if (searchResults === 0) {
+        // Show no search results message.
+        document.getElementById("noSearchResults").style = "";
+        document.getElementById("searchIssueUrl").href = generateReportIssueUrl("[Missing] " + searchString.value + " graphic pack", "The website couldn't resolve or find a graphic packs if I searched for '" + searchString.value + "'. This shouldn't be the case as it runs on Cemu [include screenshot link]. Maybe it just needs to be added as a search tag and could be found with some other tags. Thanks for checking!");
+    }
+    else {
+        document.getElementById("noSearchResults").style = "display: none;";
+    }
+}
+
+function showLoad() {
+    for (var game in gameList) {
+        addGameList(game, gameList[game].searchTags);
+    }
+}
+
+function filterUndefined(array) {
+    filteredArray = [];
+    for (i = 0; i < array.length; i++) {
+        // Remove the undefined arrays, as we can't fetch undefined.
+        if (array[i] !== undefined) filteredArray.push(array[i]);
+    }
+    return filteredArray;
+}
+
+function fetchUrl(url, options) {
+    return fetch(url, options).then(function (fetchResponse) {
+        if (!fetchResponse.ok) {
+            throw Error("A wild fetch problem appeared! It's super effective! It's Fetch Status Code™ is " + fetchResponse.statusText);
+        }
+        return fetchResponse;
+    })
+}
+
+// Different fetch functions for different files
+function fetchGithubJson(jsonUrl) { // Async, returns Promise.
+    return fetchUrl(jsonUrl, { cache: "force-cache", headers: { Accept: "application/vnd.github.v3+json" } }).then(jsonResponse => jsonResponse.json());
+}
+
+function fetchGeneralJson(jsonUrl, options) { // Async, returns Promise.
+    return fetchUrl(jsonUrl, options).then(jsonResponse => jsonResponse.json());
+}
+
+function fetchText(textUrl) { // Async, returns Promise.
+    return fetchUrl(defaultFileUrl + textUrl, { cache: "force-cache" }).then(textResponse => textResponse.text());
+}
+
+// Main Fetch Function
+function listResponses(treeEntry) {
+    // Process every entry that's passed into the gameList with it's instructions.
+    if (treeEntry.path.split("/").length - 1 == 0) { // 
+        // Found game folder, add game's array to gameList.
+        gameList[treeEntry.path] = {}; // Before storing any attributes to the gameList's file entry, make an empty gameList attributes for this game.
+        currentGame = treeEntry.path; // This is just some short some shorthand stuff.
+    }
+    else if (treeEntry.path == treeEntry.path.split("/")[0] + "/rules.txt") {
+        // Found main resolution pack, crawl (meta)data info.
+        return fetchText(treeEntry.path).then(rulesResponse => {
+            // Define some functions only used for rules.txt's.
+            function getKeyProperties(keyString) {
+                // Split name and value (and trim them). Returns an array, key name[0] and key value[1].
+                return keyString.split(/=(.+)/).map(array => array.trim());
+            }
+            function checkIntegerOrHex(ruleInstruction) {
+                if (ruleInstruction == undefined) { // Skip undefined things for rules that e.g. don't have overwriteFormats.
+                    return true;
+                }
+                ruleInstructionSingle = ruleInstruction.split(",")[0]; // Only check the first value. Don't make instructions with mixed static types like e.g. `overwriteFormat = 0x01a,input()` but include them to the instruction.
+                if (typeof Number(ruleInstructionSingle) == "number") { // Check if it's only a number or a hex value.
+                    if (!(Number.isInteger(Number(ruleInstructionSingle)) !== 0)) {
+                        // It's not an integer (or it's a 0 integer, doesn't do anything in rules.txt)
+                        console.warn("Texture rule isn't using a valid integer (found: " + ruleInstructionSingle + "), check if a number from `" + treeEntry.path + "` is a float or is 0, which Cemu ignores.");
+                    }
+                    else {
+                        return true; // Integer
+                    }
+                }
+                else {
+                    return false; // Instruction
+                }
+            }
+            function storeGameRule(textureRule) {
+                // Save instructions to this file's entry in the game folder now that the rule is finished.
+                if (!(checkIntegerOrHex(textureRule.overwriteWidth[0]) && checkIntegerOrHex(textureRule.overwriteHeight[0]) && checkIntegerOrHex(textureRule.overwriteFormat[0]))) {
+                    // Skip the rules that are entirely static values and don't need processing.
+                    // Now 'group' the instructions that belong to the same 'input'.
+
+                    // Now some information over the syntax usage in the graphic packs:
+                    // A group can consist of:
+                    // * An input bound to it (which can be bound to a group using the define method, see below).
+                    //      - Has an instruction. The instruction will run for each 'usage', and can use the attributes.
+                    //      - Has the template name for the instruction that will show up in the options panel. (Supported: checkbox, numbers, none).
+                    //      - Has the default value (an user should be able to download the packs without having to adjust anything).
+                    //      - Has description.
+                    //      - Has optional comment (advice, warning).
+                    //      - Has optional min-max values.
+                    // * Rules (simply use the group's name, e.g. 'auto' or 'gamepad').
+                    //      - Add additional attributes, e.g. 'manual(multiplier=3)'. These values can be used by the attribute name (e.g. multiplier) in the input's instruction.
+                    //      - Every rule always has it's rule's values as attributes, so there's no need to pass the width, height etc. of the current rule.
+
+                    // There's 3 instruction groups already predefined, auto, gamepad, manual.
+                }
+            }
+
+            rulesLines = rulesResponse.match(/[^\r\n]+/g);
+            currentGame = treeEntry.path.split("/")[0];
+            currentRule = new textureRule(undefined);
+            gameList[currentGame][treeEntry.path] = [];
+            indexLength = 0;
+
+            for (i = 0; i < rulesLines.length; i++) {
+                // Keeping comments, but have something to compare with. Also make a variable that holds the current line, as it'll result in overall faster read access.
+                currentLine = rulesLines[i].trim();
+                parseLine = currentLine.substr(0, -currentLine.indexOf("#") > 0 ? currentLine.length : currentLine.indexOf("#")).trim();
+
+                if (currentLine !== "") {
+                    if (parseLine.charAt(0) == '[' && parseLine.slice(-1) == ']') { // New section
+                        // First, store the textureRule before creating a new textureRule.
+                        if (currentRule.sectionName != undefined) storeGameRule(currentRule);
+                        currentRule = new textureRule(parseLine);
+                    }
+                    else if (currentRule.sectionName == "[Definition]") {
+                        // Generally, we only expect metadata in "[Definition]" section for rules.
+                        if (currentLine.charAt(0) == '#' && currentLine.charAt(1) == '#') { // Current line
+                            gameList[currentGame][getKeyProperties(currentLine.substr(2))[0]] = JSON.parse(getKeyProperties(currentLine.substr(2))[1]);
+                        }
+                        // Now only expect either a name or titleId's keys.
+                        else if (getKeyProperties(parseLine)[0] == "name") { // Game Title
+                            gameList[currentGame].gameTitle = getKeyProperties(parseLine)[1].slice(1, -1); // Removes quotes.
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "titleIds") { // Game IDs
+                            gameList[currentGame].titleIds = getKeyProperties(parseLine)[1];
+                        }
+                        else {
+                            console.warn("Unexpected rules line: `" + parseLine + "` in `" + currentRule.sectionName + "` section from `" + treeEntry.path + "`.");
+                        }
+                    }
+                    else if (currentRule.sectionName == "[Control]") {
+                        // Handle VsyncControl
+                        if (getKeyProperties(parseLine)[0] == "vsyncFrequency") {
+                            currentRule.vsyncFrequency = getKeyProperties(parseLine)[1];
+                        }
+                    }
+                    else if (currentRule.sectionName == "[TextureRedefine]") {
+                        // First, read the filters.
+                        if (getKeyProperties(parseLine)[0] == "width") {
+                            currentRule.width = getKeyProperties(parseLine)[1];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "height") {
+                            currentRule.height = getKeyProperties(parseLine)[1];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "formats") {
+                            currentRule.formats = getKeyProperties(parseLine)[1];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "formatsExcluded") {
+                            currentRule.formatsExcluded = getKeyProperties(parseLine)[1];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "tilemodes") {
+                            currentRule.tilemodes = getKeyProperties(parseLine)[1];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "tilemodesExcluded") {
+                            currentRule.tilemodesExcluded = getKeyProperties(parseLine)[1];
+                        }
+                        // Read overwrites into this format [value, index]
+                        else if (getKeyProperties(parseLine)[0] == "overwriteWidth") {
+                            currentRule.overwriteWidth = [getKeyProperties(parseLine)[1], indexLength + rulesLines[i].indexOf(getKeyProperties(parseLine)[1]), getKeyProperties(parseLine)[1].length];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "overwriteHeight") {
+                            currentRule.overwriteHeight = [getKeyProperties(parseLine)[1], indexLength + rulesLines[i].indexOf(getKeyProperties(parseLine)[1]), getKeyProperties(parseLine)[1].length];
+                        }
+                        else if (getKeyProperties(parseLine)[0] == "overwriteFormat") {
+                            currentRule.overwriteFormat = [getKeyProperties(parseLine)[1], indexLength + rulesLines[i].indexOf(getKeyProperties(parseLine)[1]), getKeyProperties(parseLine)[1].length];
+                        }
+                    }
+                }
+                indexLength += rulesLines[i].length;
+            }
+            gameList[currentGame].searchTags.unshift(gameList[currentGame].gameTitle); // Add game title to search tags, easier to pass.
+
+            return rulesResponse;
+        });
+    }
+}
+
+function listGames(treeFetchUrl) { // Fetch repository contents
+    if (self.fetch) {
+        // Fetch is supported by browser.
+        fetchGithubJson(treeFetchUrl).then(treeResponse => {
+            // Initialize new Promise, made out of every other game file.
+            Promise.all(filterUndefined(treeResponse.tree.map(listResponses/*For every entry in the fetched github json*/))).then(dataResponses => {
+                // Now that all promises are resolved (and everything is in gameList), wait for user interaction to finish.
+                showLoad();
+                titleIdString = "";
+                for (game in gameList) {
+                    titleIdString = titleIdString+(gameList[game].gameId[0]+"|");
+                }
+                ungroupedCompatRatings = new Object;
+                fetchGeneralJson("https://cors-anywhere.herokuapp.com/http://compat.cemu.info/w/api?action=query&titles="+titleIdString.slice(0, -1)+"&redirects&prop=revisions|redirects&rvprop=content&format=json", { cache: "force-cache" }).then(function (compatResponse) {
+                    // Load the fetched response to the gameList. The response has all of the titles compatibility info.
+                    for (game in compatResponse.query.pages) {
+                        if (compatResponse.query.pages[game].redirects!==undefined) ungroupedCompatRatings[compatResponse.query.pages[game].redirects[0].title] = compatResponse.query.pages[game].revisions[0]["*"].match(/(?:version=)[^|]+|(?:OS=)[^|]+|(?:region=)[^|]+|(?:CPU=)[^|]+|(?:GPU=)[^|]+|(?:user=)[^|]+|(?:FPS=)[^|]+|(?:rating=)[^|]+|(?:notes=)[^}}]/gm);
+                    }
+                });
+                console.log("All promises are resolved, succesfully loaded " + dataResponses.length + " responses!");
+            });
+        });
+    }
+    else {
+        console.warn("Fetch isn't supported (yet) for your browser. If you get this error, your browser isn't yet supported by this website.");
+    }
+}
+
+function showPreview(elementClicked) {
+    // Now process the request to the new element.
+    if (elementClicked.classList.contains("clickLabel")) {
+        currentFolderName = elementClicked.id.substr(6);
+    }
+    else if (elementClicked.id === "nextButton") {
+        currentFolderName = document.getElementById("gameList").querySelector(".checkbox:checked~.clickLabel:not(.custom)").id.substr(6);
+    }
+    // Change the preview.
+    document.getElementById("settingsSection").className = "maximized";
+    document.getElementById("gameTitle").innerText = gameList[currentFolderName].displayName;
+    // Check if it's the last option, change the next button.
+}
+
+function continueToSettings() {
+    if (selected) {
+        document.getElementById("continueButton").value = "Go back";
+        document.getElementById("actionTitle").innerText = "Select a game to view it's options.";
+        // Go to the next stage
+        document.getElementsByTagName("main")[0].classList.add("selectOptions"); // The stage switching is purely cosmetic
+    }
+    else {
+        document.getElementById("noSelectionText").className = "noSelectionText";
+        document.getElementById("gameList").className += "noSelectionCheckbox";
+    }
+}
+
+function hoverAction(hovering) {
+    if (hovering) {
+        for (var i = 0; i < document.getElementsByClassName("checkbox").length; i++) {
+            if (document.getElementsByClassName("checkbox")[i].checked) {
+                document.getElementById("continueButton").style = "background-color: rgba(158,158,158,.3);";
+                selected = true;
+                return;
+            }
+        }
+        // Reset noSelection transition (TODO: Poor design)
+        document.getElementById("noSelectionText").className = "";
+        document.getElementById("gameList").className = "";
+    }
+    selected = false;
+    document.getElementById("continueButton").style = "background-color: rgba(158,158,158,.2);";
+}
+
+function changePreviewSize() {
+    if (document.getElementById("options").scrollTop > 10) {
+        document.getElementById("settingsSection").className = "minimized";
+    }
+    else {
+        document.getElementById("settingsSection").className = "maximized";
+    }
+}
+
+function downloadBuild() {
+}
+
+listGames(defaultFetchUrl);
