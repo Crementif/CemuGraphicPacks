8edee655dfe4aa389b383782bf0acfe34838e916
Crementif <milanhekje@gmail.com>
2018-02-14 14:48:30 +0100

    Split Javascript
    Pushing many updates currently, but I don't want to stack important changes that could prevent readability of the history.
    
    Splitting the javascript will likely lead to even faster loads, which is kinda key in a website that's already gonna fetch many files, hence why I did this.
    
    This might also help if I want to make other pages that can use these building fetches, while not using those page specific buttons etc.
    
    More small changes include better html tag closement, having a viewport meta tag that'll allow better mobile browsing (I'm planning on having only 1 panel visable at all times on mobile, which probably will fix the only major bug).

---
+++ docs/js/index.js
@@ -0,0 +1,87 @@
+﻿/* Page specific functions */
+
+function generateReportIssueUrl(title, body) {
+    return reportUrl + "new?title=" + title + "&body=" + body;
+}
+
+function showPreview(elementClicked) {
+    // Now process the request to the new element.
+    if (elementClicked.classList.contains("clickLabel")) {
+        currentFolderName = elementClicked.id.substr(6);
+    }
+    else if (elementClicked.id === "nextButton") {
+        currentFolderName = document.getElementById("gameList").querySelector(".checkbox:checked~.clickLabel:not(.custom)").id.substr(6);
+    }
+    // Change the preview.
+    document.getElementById("settingsSection").className = "maximized";
+    document.getElementById("gameTitle").innerText = gameList[currentFolderName].displayName;
+    // Check if it's the last option, change the next button.
+}
+
+function filterGameList(searchString) {
+    searchResults = 0;
+    if (searchString.value.toLowerCase().indexOf("mario od") > -1) document.getElementById("logo").innerText = "SwEmu Graphic Packs"; // 'ha funny'
+    for (var game in gameList) {
+        for (i = 0; i < gameList[game].searchTags.length; i++) {
+            // Now compare it with the search string.
+            if (gameList[game].searchTags[i].toLowerCase().indexOf(searchString.value.toLowerCase()) > -1) {
+                document.getElementsByName(game)[0].parentElement.style = "";
+                searchResults += 1;
+                break; // Found 1 tag that matched. Quit looping this game entry.
+            }
+            else {
+                document.getElementsByName(game)[0].parentElement.style = "display: none;";
+            }
+        }
+    }
+    if (searchResults === 0) {
+        // Show no search results message.
+        document.getElementById("noSearchResults").style = "";
+        document.getElementById("searchIssueUrl").href = generateReportIssueUrl("[Missing] " + searchString.value + " graphic pack", "The website couldn't resolve or find a graphic packs if I searched for '" + searchString.value + "'. This shouldn't be the case as it runs on Cemu [include an optional screenshot by drag and dropping an image]. Maybe it just needs to be added as a search tag and could be found with some other tags. Thanks for checking!");
+    }
+    else {
+        document.getElementById("noSearchResults").style = "display: none;";
+    }
+}
+
+function continueToSettings() {
+    if (selected) {
+        document.getElementById("continueButton").value = "Go back";
+        document.getElementById("actionTitle").innerText = "Select a game to view it's options.";
+        // Go to the next stage
+        document.getElementsByTagName("main")[0].classList.add("selectOptions"); // The stage switching is purely cosmetic
+    }
+    else {
+        document.getElementById("noSelectionText").className = "noSelectionText";
+        document.getElementById("gameList").className += "noSelectionCheckbox";
+    }
+}
+
+function hoverAction(hovering) {
+    if (hovering) {
+        for (var i = 0; i < document.getElementsByClassName("checkbox").length; i++) {
+            if (document.getElementsByClassName("checkbox")[i].checked) {
+                document.getElementById("continueButton").style = "background-color: rgba(158,158,158,.3);";
+                selected = true;
+                return;
+            }
+        }
+        // Reset noSelection transition (TODO: Poor design)
+        document.getElementById("noSelectionText").className = "";
+        document.getElementById("gameList").className = "";
+    }
+    selected = false;
+    document.getElementById("continueButton").style = "background-color: rgba(158,158,158,.2);";
+}
+
+function changePreviewSize() {
+    if (document.getElementById("options").scrollTop > 10) {
+        document.getElementById("settingsSection").className = "minimized";
+    }
+    else {
+        document.getElementById("settingsSection").className = "maximized";
+    }
+}
+
+function downloadBuild() {
+}
